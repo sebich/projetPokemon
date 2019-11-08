b6c97594e00c32cc41146fe9cc9ce9d7f84b6fa8
sebich <seb082011@gmail.com>
2019-11-08 17:38:31 +0100

    modification fichier auth.service.ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/delay';
+// RxJS 6
+import { Observable, of } from 'rxjs';
+import { tap, delay } from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
 	isLoggedIn: boolean = false; // L'utilisateur est-il connecté ?
 	redirectUrl: string; // où rediriger l'utilisateur après l'authentification ?
-
 	// Une méthode de connexion
 	login(name: string, password: string): Observable<boolean> {
 		// Faites votre appel à un service d'authentification...
 		let isLoggedIn = (name === 'admin' && password === 'admin');
 
-		return Observable.of(true).delay(1000).do(val => this.isLoggedIn = isLoggedIn);
+		return of(true).pipe(
+			delay(1000),
+			tap(val => this.isLoggedIn = isLoggedIn)
+		);
 	}
 
 	// Une méthode de déconnexion
